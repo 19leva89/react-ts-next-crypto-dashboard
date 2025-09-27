cb7506b11c788e9c9f387979fa70d080105f9547
Sobolev Dmitry <19leva89@gmail.com>
2025-09-27 17:53:48 +0300

    upd test files for hooks + upd test files for lib

--- src/hooks/use-format-value.ts
+++ src/hooks/use-format-value.ts
@@ -9,7 +9,7 @@ export const useFormatValue = () => {
 		const absValue = Math.abs(value)
 		const isLargeNumber = absValue >= 1
 
-		const formatted = value.toLocaleString(locale, {
+		const formatter = new Intl.NumberFormat(locale, {
 			style: 'decimal',
 			useGrouping: useGrouping ?? false,
 			minimumFractionDigits: isLargeNumber ? 2 : 1,
@@ -17,16 +17,14 @@ export const useFormatValue = () => {
 			notation: 'standard',
 		})
 
-		let customFormatted = formatted
-			.replace(/\u00A0/g, ' ') // If there is a non-breaking space
-			.replace(/\u202f/g, ' ') // Narrow no-break space for some locales like fr-FR
-			.replace(/,/g, ' ') // Thousands separator
-			.replace(/\./g, ',') // Fraction separator
+		const parts = formatter.formatToParts(value)
 
-		if (isLargeNumber && locale.startsWith('fr-')) {
-			customFormatted = customFormatted.replace(/ (\d{2})$/, ',$1')
-		}
-
-		return customFormatted
+		return parts
+			.map((p) => {
+				if (p.type === 'group') return ' ' // Thousands separator
+				if (p.type === 'decimal') return ',' // Fraction separator
+				return p.value.replace(/\u00A0|\u202f/g, ' ') // If there is a non-breaking space
+			})
+			.join('')
 	}
 }
